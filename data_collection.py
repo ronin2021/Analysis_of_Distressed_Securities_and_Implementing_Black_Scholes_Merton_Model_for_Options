040587a4523cdf6a5288640967d82031427c0c41
Matthew Willson <83780964+MattDWillson@users.noreply.github.com>
2021-07-10 08:46:37 -0400

    Update data_collection.py

--- data_collection.py
+++ data_collection.py
@@ -6,10 +6,3 @@ import panel as pn
 from panel.interact import interact
 import plotly.express as px
 pn.extension("plotly")
-
-#Read in Marriot and Hilton CSV files
-
-csvpath = Path("../hilton- Sheet1.csv")
-hilton_price_df = pd.read_csv(csv_path, index_col="Date", inder_datetime_format=True, parse_dates=True)
-hilton_price_df.sort_index(inplace=True)
-hilton_price_df.head()
